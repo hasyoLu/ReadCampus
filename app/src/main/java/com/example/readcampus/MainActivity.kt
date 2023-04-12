4c3856313d039b21383459acb07f6fc60ed45521
hasyo <2101297547@qq.com>
2023-04-12 14:54:36 +0800

    develop BottomNavigationView

--- app/src/main/java/com/example/readcampus/MainActivity.kt
+++ app/src/main/java/com/example/readcampus/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_bookCity, R.id.navigation_bookshelf, R.id.navigation_my
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
