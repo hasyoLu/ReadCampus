88429e6047575b6b9b29f388654c729c3ec940fd
xuan567 <572351128@qq.com>
2023-04-16 19:20:02 +0800

    dev search ui

--- app/src/main/java/com/example/readcampus/ui/bookCity/BookCityFragment.kt
+++ app/src/main/java/com/example/readcampus/ui/bookCity/BookCityFragment.kt
@@ -1,42 +1,62 @@
 package com.example.readcampus.ui.bookCity
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.readcampus.databinding.MainFragmentBookcityBinding
+import com.example.readcampus.R
+import com.example.readcampus.placeholder.PlaceholderContent
 
+/**
+ * A fragment representing a list of Items.
+ */
 class BookCityFragment : Fragment() {
 
-    private var _binding: MainFragmentBookcityBinding? = null
+    private var columnCount = 1
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(BookCityViewModel::class.java)
-
-        _binding = MainFragmentBookcityBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
         }
-        return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_book_city_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = MyBookItemRecyclerViewAdapter(PlaceholderContent.ITEMS)
+            }
+        }
+        return view
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            BookCityFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
     }
 }
